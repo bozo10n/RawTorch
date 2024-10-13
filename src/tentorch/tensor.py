b414310d0cfcc38f3057eed7880b951a096d4eb0
bozoten <shridharapavel@gmail.com>
2024-10-13 17:27:10 -0400

    MatMul added

--- src/tentorch/tensor.py
+++ src/tentorch/tensor.py
@@ -3,10 +3,10 @@ import numpy as np
 class Tensor:
     def __init__(self, data):
         self.data = np.array(data)
+        self.shape = self.data.shape
 
     def __repr__(self):
-        return f"{self.data}"
-
+        return f"Tensor({self.data})"
 
     def __add__(self, other):
         # to ensure other.data is a tensor
@@ -14,3 +14,15 @@ class Tensor:
             return Tensor(self.data + other.data)  
 
         return Tensor(self.data + other)      
+    
+    def __mul__(self, other):
+        if isinstance(other, Tensor):
+            return Tensor(self.data * other.data)
+        
+        return Tensor(self.data * other)
+    
+    def matmul(self, other):
+        if isinstance(other, Tensor):
+            return Tensor(np.dot(self.data, other.data))
+        
+        return Tensor(np.dot(self.data, other.data))
--- src/test.py
+++ src/test.py
@@ -1,8 +1,8 @@
 from tentorch import Tensor
 
-a = Tensor([1, 2, 3])
-b = Tensor([4, 5, 6])
+a = Tensor([[1, 2], [3, 4]])
+b = Tensor([[5, 6], [7, 8]])
 
-c = a + b
+c = Tensor.matmul(a, b)
 
 print(c)
