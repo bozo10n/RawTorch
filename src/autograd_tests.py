87901b00a88912534453f4914388c43616f3fdaa
bozoten <shridharapavel@gmail.com>
2024-10-28 21:04:30 -0400

    Autograd for all tensor operations and unit tests

---
+++ src/autograd_tests.py
@@ -0,0 +1,76 @@
+import unittest
+import numpy as np
+from tentorch import Tensor
+
+class TestTensor(unittest.TestCase):
+
+    def test_tensor_initialization_and_representation(self):
+        tensor = Tensor([1, 2, 3])
+        self.assertTrue(np.array_equal(tensor.data, np.array([1, 2, 3])))
+        self.assertEqual(tensor.shape, (3,))
+        self.assertEqual(repr(tensor), "Tensor([1. 2. 3.])")
+
+    def test_addition(self):
+        a = Tensor([1, 2, 3], requires_grad=True)
+        b = Tensor([3, 4, 5], requires_grad=True)
+        c = a + b
+        self.assertTrue(np.array_equal(c.data, np.array([4, 6, 8])))
+
+    def test_addition_backward(self):
+        a = Tensor([1, 2, 3], requires_grad=True)
+        b = Tensor([3, 4, 5], requires_grad=True)
+        c = a + b
+        c.backward()
+        self.assertTrue(np.array_equal(a.grad, np.ones_like(a.data)))
+        self.assertTrue(np.array_equal(b.grad, np.ones_like(b.data)))
+
+    def test_subtraction(self):
+        a = Tensor([5, 7, 9], requires_grad=True)
+        b = Tensor([1, 2, 3], requires_grad=True)
+        c = a - b
+        self.assertTrue(np.array_equal(c.data, np.array([4, 5, 6])))
+
+    def test_subtraction_backward(self):
+        a = Tensor([5, 7, 9], requires_grad=True)
+        b = Tensor([1, 2, 3], requires_grad=True)
+        c = a - b
+        c.backward()
+        self.assertTrue(np.array_equal(a.grad, np.ones_like(a.data)))
+        self.assertTrue(np.array_equal(b.grad, -np.ones_like(b.data)))
+
+    def test_multiplication(self):
+        a = Tensor([1, 2, 3], requires_grad=True)
+        b = Tensor([4, 5, 6], requires_grad=True)
+        c = a * b
+        self.assertTrue(np.array_equal(c.data, np.array([4, 10, 18])))
+
+    def test_multiplication_backward(self):
+        a = Tensor([1, 2, 3], requires_grad=True)
+        b = Tensor([4, 5, 6], requires_grad=True)
+        c = a * b
+        c.backward()
+        self.assertTrue(np.array_equal(a.grad, b.data))
+        self.assertTrue(np.array_equal(b.grad, a.data))
+
+    def test_broadcasting(self):
+        a = Tensor([[1, 2], [3, 4]], requires_grad=True)
+        b = Tensor([10, 20], requires_grad=True)
+        broadcasted_b = a.broadcast(b)
+        self.assertTrue(np.array_equal(broadcasted_b, np.array([[10, 20], [10, 20]])))
+
+    def test_matmul(self):
+        a = Tensor([[1, 2], [3, 4]], requires_grad=True)
+        b = Tensor([[5, 6], [7, 8]], requires_grad=True)
+        c = a.matmul(b)
+        self.assertTrue(np.array_equal(c.data, np.array([[19, 22], [43, 50]])))
+
+    def test_matmul_backward(self):
+        a = Tensor([[1, 2], [3, 4]], requires_grad=True)
+        b = Tensor([[5, 6], [7, 8]], requires_grad=True)
+        c = a.matmul(b)
+        c.backward()
+        self.assertTrue(np.array_equal(a.grad, np.dot(np.ones_like(c.data), b.data.T)))
+        self.assertTrue(np.array_equal(b.grad, np.dot(a.data.T, np.ones_like(c.data))))
+
+if __name__ == "__main__":
+    unittest.main()
--- src/tentorch/tensor.py
+++ src/tentorch/tensor.py
@@ -2,13 +2,14 @@ import numpy as np
 from numpy import dtype, float64, broadcast_to
 
 class Tensor:
-    def __init__(self, data, requires_grad=False):
+    def __init__(self, data, requires_grad=False, name=None):
         self.data = np.array(data, dtype=float64)
         self.shape = self.data.shape
         self.requires_grad= requires_grad
         self.grad = None
         self._backward = None
         self._prev = []
+        self._name = name or f"Tensor_{id(self)}"
 
     def __repr__(self):
         return f"Tensor({self.data})"
@@ -37,32 +38,89 @@ class Tensor:
 
     def __add__(self, other):
         # to ensure other.data is a tensor
-        other = self.broadcast(other)
         if isinstance(other, Tensor):
-            return Tensor(self.data + other.data)  
+            out = Tensor(self.data + other.data, requires_grad=self.requires_grad or other.requires_grad)
+        else:
+            out = Tensor(self.data + other, requires_grad=self.requires_grad or other)
+        
 
-        return Tensor(self.data + other)
+        def _backward():
+            if self.grad is None:
+                self.grad = np.zeros_like(self.data)
+
+            if other.grad is None:
+                other.grad = np.zeros_like(other.data)
+
+            self.grad += np.ones_like(self.data) * out.grad
+            other.grad += np.ones_like(other.data) * out.grad
+
+        out._backward = _backward
+        out._prev = [self, other]
+
+        return out
 
     def __sub__(self, other):
         # to ensure other.data is a tensor
-        other = self.broadcast(other)
         if isinstance(other, Tensor):
-            return Tensor(self.data - other.data)  
+            out = Tensor(self.data - other.data, requires_grad=self.requires_grad or other.requires_grad)
+        else:
+            out = Tensor(self.data - other, requires_grad=self.requires_grad or other)
 
-        return Tensor(self.data - other)            
+        def _backward():
+            if self.grad is None:
+                self.grad = np.zeros_like(self.data)
+
+            if other.grad is None:
+                other.grad = np.zeros_like(other.data)
+
+            self.grad += np.ones_like(self.data) * out.grad
+            other.grad += -np.ones_like(other.data) * out.grad
+
+        out._backward = _backward
+
+        out._prev = [self, other]
+
+        return out                
     
     def __mul__(self, other):
-        other.data = self.broadcast(other)
         if isinstance(other, Tensor):
-            return Tensor(self.data * other.data)
-        
-        return Tensor(self.data * other)
+            out = Tensor(self.data * other.data, requires_grad=self.requires_grad or other.requires_grad)
+        else:
+            out = Tensor(self.data * other, requires_grad=self.requires_grad or other)
+
+        def _backward():
+            if self.grad is None:
+                self.grad = np.zeros_like(self.data)
+            if other.grad is None:
+                other.grad = np.zeros_like(other.data)
+
+            self.grad += other.data * out.grad # in respect to self
+            other.grad += self.data * out.grad # in respect to out
+
+        out._backward = _backward
+        out._prev = [self, other]
+
+        return out             
     
     def matmul(self, other):
         if isinstance(other, Tensor):
-            return Tensor(np.dot(self.data, other.data))
-        
-        return Tensor(np.dot(self.data, other.data))
+            out = Tensor(np.dot(self.data, other.data), requires_grad=self.requires_grad or other.requires_grad)
+        else:
+            out = Tensor(np.dot(self.data, other), requires_grad=self.requires_grad or other)
+
+        def _backward():
+            if self.grad is None:
+                self.grad = np.zeros_like(self.data)
+            if other.grad is None:
+                other.grad = np.zeros_like(other.data)    
+
+            self.grad += np.dot(out.grad, other.data.T)    
+            other.grad += np.dot( self.data.T, out.grad)
+
+        out._backward = _backward
+        out._prev = [self, other]
+
+        return out    
     
     def randn(shape):
         return Tensor(np.random.randn(shape))
